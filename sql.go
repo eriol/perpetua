0e444d9de6f9928c1f905b0e8affa264c5cc973e
Daniele Tricoli <eriol@mornie.org>
2014-05-15 01:17:15 +0200

    Initial implementation of a store object

---
+++ sql.go
@@ -0,0 +1,46 @@
+package perpetua
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func (s *Store) open() {
+	db, err := sql.Open("sqlite3", DATABASE_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.db = db
+}
+
+func (s *Store) close() {
+	s.db.Close()
+}
+
+func (s *Store) createDatabase() {
+
+	// TODO: create all tables in one variable when multiple statements will
+	// be supported see https://github.com/mattn/go-sqlite3/issues/60
+	sql_people_table := `CREATE TABLE people (
+	id INTEGER NOT NULL PRIMARY KEY autoincrement,
+	name TEXT
+);
+	`
+	sql_quotes_table := `CREATE TABLE quotes (
+	person_id INTEGER NOT NULL,
+	quote TEXT,
+	FOREIGN KEY(person_id) REFERENCES people(id)
+);
+	`
+	s.db.Exec(sql_people_table)
+	s.db.Exec(sql_quotes_table)
+
+}
+
+var store Store
