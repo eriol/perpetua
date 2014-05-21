5c376e61338676de73a7ce498fdbac459aab9bcf
Daniele Tricoli <eriol@mornie.org>
2014-05-21 18:25:28 +0200

    Added alias table

--- db/sql.go
+++ db/sql.go
@@ -37,23 +37,42 @@ func (s *Store) createDatabase() {
 	sql_people_table := `CREATE TABLE people (
 	id INTEGER NOT NULL PRIMARY KEY autoincrement,
 	name TEXT
-);
-	`
+);`
+
 	sql_quotes_table := `CREATE TABLE quotes (
 	id INTEGER NOT NULL PRIMARY KEY autoincrement,
 	person_id INTEGER NOT NULL,
 	quote TEXT,
 	FOREIGN KEY(person_id) REFERENCES people(id)
-);
-	`
+);`
+
+	sql_alias_table := `CREATE TABLE alias (
+	person_id INTEGER NOT NULL,
+	alias TEXT,
+	FOREIGN KEY(person_id) REFERENCES people(id)
+);`
+
 	s.db.Exec(sql_people_table)
 	s.db.Exec(sql_quotes_table)
+	s.db.Exec(sql_alias_table)
+}
+
+func (s *Store) getPerson(name string) (id int) {
+	query_people := `SELECT id FROM people WHERE name = ?`
+	query_alias := `SELECT person_id FROM alias WHERE alias = ?`
+
+	s.db.QueryRow(query_people, name).Scan(&id)
+	if id == 0 {
+		s.db.QueryRow(query_alias, name).Scan(&id)
+	}
+
+	return
 }
 
 func (s *Store) GetQuote(person string) (quote string) {
 	var q string
-	query := `SELECT quote FROM quotes WHERE person_id =
-		(SELECT id FROM people WHERE name = ?) ORDER BY RANDOM() LIMIT 1;`
-	s.db.QueryRow(query, person).Scan(&q)
+	query := `SELECT quote FROM quotes WHERE person_id = ?
+		ORDER BY RANDOM() LIMIT 1;`
+	s.db.QueryRow(query, s.getPerson(person)).Scan(&q)
 	return q
 }
