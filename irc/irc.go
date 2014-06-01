b5f4963fb4be10f95263ad22eb963364ed01b230
Daniele Tricoli <eriol@mornie.org>
2014-06-01 05:21:44 +0200

    Add copyright note and license information

--- irc/irc.go
+++ irc/irc.go
@@ -1,3 +1,8 @@
+// Copyright © 2014 Daniele Tricoli <eriol@mornie.org>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package irc
 
 import (
