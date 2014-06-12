3172e8ff53eb825b3c070a8a5c49510be92ab5a3
Daniele Tricoli <eriol@mornie.org>
2014-06-12 02:16:11 +0200

    Add tests for config

---
+++ config/options_test.go
@@ -0,0 +1,95 @@
+// Copyright © 2014 Daniele Tricoli <eriol@mornie.org>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package config
+
+import "testing"
+
+// Check if default values are set correctly.
+func TestDefaultValues(t *testing.T) {
+
+	var option Options
+	cfg := ""
+
+	option.ReadFromString(cfg)
+
+	if option.IRC.Nickname != DEFAULT_NICKNAME {
+		t.Error("Default nickname not set correctly!")
+	}
+
+	if option.IRC.User != DEFAULT_USER {
+		t.Error("Default user not set correctly!")
+	}
+
+	if option.I18N.Lang != DEFAULT_LANG {
+		t.Error("Default language not set correctly!")
+	}
+}
+
+func TestChannels(t *testing.T) {
+
+	var option Options
+	cfg := `[IRC]
+	channel = channel1
+	channel = "#channel2"
+	`
+
+	option.ReadFromString(cfg)
+
+	if option.IRC.Channel[0] != "#channel1" &&
+		option.IRC.Channel[1] != "#channel2" {
+		t.Error("Channels not set correctly!")
+	}
+
+}
+
+func TestConfigExample(t *testing.T) {
+
+	var option Options
+	cfg := `
+	[Server]
+	hostname = irc.example.org
+	port = 9999
+	useTLS = true
+	skipVerify = false
+	[IRC]
+	nickname = perpetua-test
+	channel = test
+	channel = "#test1"
+	[I18N]
+	lang = it
+	`
+
+	option.ReadFromString(cfg)
+
+	if option.Server.Hostname != "irc.example.org" {
+		t.Error("Server hostname not set correctly!")
+	}
+
+	if option.Server.Port != 9999 {
+		t.Error("Server port not set correctly!")
+	}
+
+	if option.Server.UseTLS != true {
+		t.Error("Option useTSL not set correctly!")
+	}
+
+	if option.Server.SkipVerify != false {
+		t.Error("Option skipVerify not set correctly!")
+	}
+
+	if option.IRC.Nickname != "perpetua-test" {
+		t.Error("Bot nickname not set correctly!")
+	}
+
+	if option.IRC.Channel[0] != "#test" &&
+		option.IRC.Channel[1] != "#test1" {
+		t.Error("Channels not set correctly!")
+	}
+
+	if option.I18N.Lang != "it" {
+		t.Error("Lang not set correctly!")
+	}
+}
