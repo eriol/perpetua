fd03ab3a44d6314c4145059e024f2d312828a411
Daniele Tricoli <eriol@mornie.org>
2015-08-09 19:12:43 +0200

    Update copyright year! :cake:

--- config/config_test.go
+++ config/config_test.go
@@ -1,4 +1,4 @@
-// Copyright © 2014 Daniele Tricoli <eriol@mornie.org>.
+// Copyright © 2014-2015 Daniele Tricoli <eriol@mornie.org>.
 // All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
